102d66f3fbced698f843c1559cafe5062a8e9dbc
e <gegewu1051@163.com>
2017-07-02 18:42:56 +0800

    del

--- mycarserver/MyCar2/MyCar/app/src/main/java/fancy/mycar/Constant.java
+++
@@ -1,9 +0,0 @@
-package fancy.mycar;
-
-import android.content.Context;
-import android.os.Handler;
-
-public class Constant {
-    public static Context context;
-    public static Handler handler = null;
-}
